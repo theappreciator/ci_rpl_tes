b2fe22647136db0db4a2433d100b20300571cc5f
Jess Tucker <jessjesstheappreciator@gmail.com>
2017-08-03 09:06:51 -0400

    Add Person test

---
+++ tests/PersonTest.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'lib/person.php'
+
+class PersonTest extends PHPUnit_FrameworkTestCase
+{
+	public $personInstance;
+	
+	public function setUp() {
+		$this->personInstance = new Person();
+	}
+	
+	public function testIfNameWorks() {
+		$this->personInstance->setName("Jon", "Snow");
+		
+		$this->assertEquals("Jon Snow", $this->personInstance->getName());
+	}
+}
