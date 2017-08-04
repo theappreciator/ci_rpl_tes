d6eea2b1ac72cef7647a58f56f6bf197e48dc32b
Jess Tucker <jessjesstheappreciator@gmail.com>
2017-08-04 09:50:12 -0400

    Fix test typo

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -11,18 +11,26 @@ class PersonTest extends PHPUnit_Framework_TestCase
 	}
 	
 	public function testIfNameWorks() {
+		echo "Testing the name functions of Person";
+		
+		echo "Setting name to Jon Snow";
 		$this->personInstance->setName("Jon", "Snow");
 		
 		$this->assertEquals("Jon Snow", $this->personInstance->getName());
+		echo "Finished asserting that name was Jon Snow";
 	}
 	
 	public function testAddChild() {
-		$child = new Person("Test", "Child");
+		echo "Testing the child functions of a Person";
 		
 		$this->assertEquals(0, count($this->personInstance->getChildren()));
+		echo "Finished asserting that the initial child count of Person is 0";
 		
+		echo "Adding a test child";
+		$child = new Person("Test", "Child");
 		$this->personInstance->addChild($child);
 		
 		$this->assertEquals(1, count($this->personInstance->getChildren()));
+		echo "Finished asserting that the child count is now 1";
 	}	
 }
