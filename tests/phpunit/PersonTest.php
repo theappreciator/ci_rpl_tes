d34ff110bc01c58fe57ddc6bfe0d9b249a29fe64
Jess Tucker <jessjesstheappreciator@gmail.com>
2017-08-14 17:18:27 -0400

    Change PersonTest to use different test code convention

--- tests/phpunit/PersonTest.php
+++ tests/phpunit/PersonTest.php
@@ -2,7 +2,9 @@
 
 require_once 'lib/person.php';
 
-class PersonTest extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class PersonTest extends TestCase
 {
 	public $personInstance;
 	
